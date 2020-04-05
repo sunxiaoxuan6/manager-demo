2a03578e3d3837c8249bea661a17c2217cf27697
sunxiaoxuan6 <1657394323@qq.com>
2020-04-05 20:37:38 +0800

    整理代码

--- src/main/java/com/niit/managerdemo/controller/MessageController.java
+++ src/main/java/com/niit/managerdemo/controller/MessageController.java
@@ -13,12 +13,15 @@ public class MessageController {
     @Autowired
     private MessageRepository messageRepository;
 
+//    查询所有
     @GetMapping("/findAll/{page}/{size}")
-    public Page<Message> findAll(@PathVariable("page") Integer page,@PathVariable("size") Integer size){
+    public Page<Message> findAll(@PathVariable("page") Integer page,
+                                 @PathVariable("size") Integer size){
         PageRequest request=PageRequest.of(page,size);
         return messageRepository.findAll(request);
     }
 
+//    添加
     @PostMapping("/save")
     public String save(@RequestBody Message message){
         Message result = messageRepository.save(message);
@@ -29,11 +32,13 @@ public class MessageController {
         }
     }
 
+
     @GetMapping("/findById/{id}")
     public Message findById(@PathVariable("id") Integer id){
         return messageRepository.findById(id).get();
     }
 
+//    修改(RequestBody：传递数据)
     @PutMapping("/update")
     public String update(@RequestBody Message message){
         Message result = messageRepository.save(message);
@@ -44,6 +49,7 @@ public class MessageController {
         }
     }
 
+//    删除(PathVariable:接收id)
     @DeleteMapping("/deleteById/{id}")
     public void deleteById(@PathVariable("id") Integer id){
         messageRepository.deleteById(id);
